ebcb815200125bbae790db27890211b2c1529a1f
Rickson <rickson.simoes@hotmail.com>
2021-05-20 23:05:06 -0300

    :wrench: :pencil: :hammer: Middleware/ :hammer: Get statement

--- src/index.js
+++ src/index.js
@@ -7,9 +7,22 @@ app.listen(3333);
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  
+  if (!customer) {
+    return response.status(400).json({error: 'Customer not found.'})
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (request, response) => {
   const {cpf, name} = request.body;
-
   const customerAlreadyExists = customers.some( customer => customer.cpf === cpf);
 
   if (customerAlreadyExists) {
@@ -24,4 +37,12 @@ app.post('/account', (request, response) => {
   });
 
   return response.status(201).send();
+})
+
+// app.use(verifyIfExistsAccountCPF);
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.status(200).json(customer.statement);
 })
